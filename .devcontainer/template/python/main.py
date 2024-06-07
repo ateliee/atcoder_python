81b047f38b176705d427e7bd7c44492b5a1f53a3
ateliee <ateliee@gmail.com>
2024-06-07 23:29:22 +0900

    fix: テンプレート調整

--- .devcontainer/template/python/main.py
+++ .devcontainer/template/python/main.py
@@ -1,11 +1,11 @@
-import bisect,collections,copy,heapq,itertools,math,numpy,string
+# import bisect,collections,copy,heapq,itertools,math,numpy,string
 import sys
-def I(): return int(sys.stdin.readline().rstrip())
+# def I(): return int(sys.stdin.readline().rstrip())
 def LI(): return list(map(int,sys.stdin.readline().rstrip().split()))
-def S(): return sys.stdin.readline().rstrip()
-def LS(): return list(sys.stdin.readline().rstrip().split())
-# get inputs
-N = I()
-A = [LI() for _ in range(N)]
+# def S(): return sys.stdin.readline().rstrip()
+# def LS(): return list(sys.stdin.readline().rstrip().split())
+# # get inputs
+N = LI()
+# A = [LI() for _ in range(N)]
 # output
-print(A)
+print(N)
