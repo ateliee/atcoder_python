3bfd0655b40da06086d76c48125c668bf9a03480
ateliee <ateliee@gmail.com>
2024-06-07 21:34:50 +0900

    fix: テンプレートの調整

--- template.py
+++ template.py
@@ -4,5 +4,8 @@ def I(): return int(sys.stdin.readline().rstrip())
 def LI(): return list(map(int,sys.stdin.readline().rstrip().split()))
 def S(): return sys.stdin.readline().rstrip()
 def LS(): return list(sys.stdin.readline().rstrip().split())
+# get inputs
 N = I()
 A = [LI() for _ in range(N)]
+# output
+print(A)
